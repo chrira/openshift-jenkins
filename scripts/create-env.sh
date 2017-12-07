969ec7bf40ab44d9a59de48055b59de4165a3530
Christoph Raaflaub <raaflaub@puzzle.ch>
2017-12-07 14:42:31 +0100

    update build configuration

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
+
 # configuration script for the OpenShift environment
 
-oc process -f jenkins-ephemeral-template.json \
-  | oc create -f -
 
+oc new-project craaflaub-jenkins
+
+oc process -f jenkins-ephemeral-template.json \
+  | oc apply -f -
+
+
+oc start-build gretl --from-dir . --follow
+
+openshift/jenkins-slave-maven-centos7
+
+
+
+############################
+
+
+oc process -f jenkins-s2i-template.json \
+  | oc apply -f -
 
 #  -p DB_RESET_DATA=true \
+
+
+oc create -f build.template
