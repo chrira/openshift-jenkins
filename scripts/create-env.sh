7d9de129669add6fbf24a9a2761af6d0eea269a1
Christoph Raaflaub <raaflaub@puzzle.ch>
2017-12-11 14:43:31 +0100

    read job repo url from environment

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -1,36 +1,12 @@
 #!/bin/sh
 
-
 # configuration script for the OpenShift environment
-
-
-oc new-project craaflaub-jenkins2
-
+#oc new-project craaflaub-jenkins
 
 oc process -f jenkins-s2i-template.json \
+  -p JENKINS_CONFIGURATION_REPO_URL="https://github.com/chrira/openshift-jenkins.git" \
+  -p GRETL_JOB_REPO_URL="git://github.com/chrira/gretljobs.git" \
   | oc apply -f -
 
-
-
-oc new-project craaflaub-jenkins
-
-oc process -f jenkins-ephemeral-template.json \
-  | oc apply -f -
-
-
-oc start-build gretl --from-dir . --follow
-
-openshift/jenkins-slave-maven-centos7
-
-
-
-############################
-
-
-oc process -f jenkins-s2i-template.json \
-  | oc apply -f -
-
-#  -p DB_RESET_DATA=true \
-
-
-oc create -f build.template
+# oc start-build gretl --from-dir . --follow
+# openshift/jenkins-slave-maven-centos7
